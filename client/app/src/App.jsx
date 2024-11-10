476f525f04b1b787f45e9e2c5cacf52104f33f80
Ranjan Joshi <ranjanjoshi17@gmail.com>
2024-11-10 14:26:25 +0545

    GET and POST done

---
+++ client/app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1>Book Website</h1>
+     <div>
+     <input type='text' placeholder='Book Title...'></input>
+     <input type='number' placeholder='Release Date....'></input>
+     <button>Add Book</button>
+     </div>
+    </>
+  )
+}
+
+export default App
